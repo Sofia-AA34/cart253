b8d0bfafb99c48b50f6ba2988db114f25b00cedc
Sofia-AA34 <s_allash@live.concordia.ca>
2026-09-23 18:49:24 -0400

    created butterfly's left and right wings (upper and lower)

--- prototypes/instructions/instructions-prototype-1/js/script.js
+++ prototypes/instructions/instructions-prototype-1/js/script.js
@@ -35,6 +35,7 @@ function draw() {
  */
 function drawButterfly() {
     drawBody();
+    drawWings();
 }
 
 /**
@@ -42,15 +43,16 @@ function drawButterfly() {
  */
 function drawBody() {
     //Butterfly's head
-    stroke(50);
     push();
-    fill(80)
+    stroke(50);
+    fill(80);
     circle(385, 161, 15);
     circle(375, 177, 25);
     pop();
 
     //Butterfly's middle 1
     push();
+    stroke(50);
     fill(80);
     translate(355, 213);
     rotate(25);
@@ -59,6 +61,7 @@ function drawBody() {
 
     //Butterfly's middle 2
     push();
+    stroke(50);
     fill(80);
     translate(335, 250);
     rotate(25);
@@ -67,6 +70,7 @@ function drawBody() {
 
     //Butterfly's bottom 1
     push();
+    stroke(50);
     fill(80);
     translate(315, 290);
     rotate(25);
@@ -75,9 +79,39 @@ function drawBody() {
 
     //Butterfly's bottom 2
     push();
+    stroke(50);
     fill(80);
     translate(293, 336);
     rotate(25);
     ellipse(0, 0, 15, 65)
+    pop();
+}
+
+/**
+ * Draw butterfly's wings 
+ */
+function drawWings() {
+    //Butterfly's right upper wing
+    push();
+    fill(255, 95, 31);
+    quad(356, 210, 620, 150, 570, 340, 355, 235);
+    pop();
+
+    //Butterfly's right lower wing
+    push();
+    fill(255, 95, 31);
+    quad(355, 235, 590, 350, 420, 460, 315, 290);
+    pop();
+
+    //Butterfly's left upper wing
+    push();
+    fill(255, 95, 31);
+    quad(220, 30, 356, 210, 345, 225, 145, 195);
+    pop();
+
+    //Butterfly's left lower wing
+    push();
+    fill(255, 95, 31);
+    quad(130, 185, 345, 225, 315, 290, 180, 350);
     pop();
 }
