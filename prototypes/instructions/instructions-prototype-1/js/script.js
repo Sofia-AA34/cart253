84ba2b3ed3e493a288e36774c3e89f4a6e3569f0
Sofia-AA34 <s_allash@live.concordia.ca>
2026-09-23 17:55:35 -0400

    created butterfly's body (head, middle and bottom)

--- prototypes/instructions/instructions-prototype-1/js/script.js
+++ prototypes/instructions/instructions-prototype-1/js/script.js
@@ -13,12 +13,71 @@
 */
 function setup() {
     createCanvas(640, 480);
+    angleMode(DEGREES);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the butterfly
 */
 function draw() {
+    // Green background
+    background(0, 155, 120);
 
+    // No stroke everywhere 
+    noStroke();
+
+    drawButterfly();
+}
+
+/**
+ * Draws the butterfly using functions
+ */
+function drawButterfly() {
+    drawBody();
+}
+
+/**
+ * Draws the butterfly's body 
+ */
+function drawBody() {
+    //Butterfly's head
+    stroke(50);
+    push();
+    fill(80)
+    circle(385, 161, 15);
+    circle(375, 177, 25);
+    pop();
+
+    //Butterfly's middle 1
+    push();
+    fill(80);
+    translate(355, 213);
+    rotate(25);
+    ellipse(0, 0, 40, 65);
+    pop();
+
+    //Butterfly's middle 2
+    push();
+    fill(80);
+    translate(335, 250);
+    rotate(25);
+    ellipse(0, 0, 34, 58);
+    pop();
+
+    //Butterfly's bottom 1
+    push();
+    fill(80);
+    translate(315, 290);
+    rotate(25);
+    ellipse(0, 0, 23, 60)
+    pop();
+
+    //Butterfly's bottom 2
+    push();
+    fill(80);
+    translate(293, 336);
+    rotate(25);
+    ellipse(0, 0, 15, 65)
+    pop();
 }
