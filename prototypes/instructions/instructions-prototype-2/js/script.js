59924727a5f877d40d80b1007bfc335acd4ed33f
Sofia-AA34 <s_allash@live.concordia.ca>
2026-09-24 01:26:43 -0400

    began the prototype project - filled out my name, added a background and drew the iris

--- prototypes/instructions/instructions-prototype-2/js/script.js
+++ prototypes/instructions/instructions-prototype-2/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Title of Project
- * Author Name
+ * Sofia Allashukurova
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -9,16 +9,37 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
+    createCanvas(400, 400);
+}
+
+/**
+ * Draws the iris
+*/
+function draw() {
+    //black background
+    background(0, 0, 0);
+
+    drawEye()
 
 }
 
+/**
+ * Draws the eye using functions 
+ */
+function drawEye() {
+    drawIris();
+
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
-
+ * Draw the iris of the eye 
+ */
+function drawIris() {
+    push();
+    fill(128, 128, 128);
+    circle(200, 200, 400);
+    pop();
 }
