0fa17c7f5e85d6074f939dabd687eda4a16768c3
Sofia-AA34 <s_allash@live.concordia.ca>
2026-09-24 18:15:31 -0400

    started the project prototype and created the shape of the character

--- prototypes/instructions/instructions-prototype-3/js/script.js
+++ prototypes/instructions/instructions-prototype-3/js/script.js
@@ -9,16 +9,77 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+draws the fugly character 
 */
 function draw() {
+    //orange background 
+    background(255, 153, 0);
+
+    drawCharacter();
+}
+
+/**
+ * Draws the character using functions
+ */
+function drawCharacter() {
+    drawShape();
+    drawEyes();
 
 }
+
+/**
+ * Draws the shape of the character 
+ */
+
+//draw the initial shape (circle)
+function drawShape() {
+    push();
+    noStroke();
+    fill('white');
+    circle(200, 200, 200);
+    pop();
+
+    //mold the circle into a better shape for the character
+    //rotate the shapes by 1/8 turn
+
+    push();
+    translate(275, 125);
+    rotate(QUARTER_PI);
+    noStroke();
+    fill('white');
+    ellipse(0, 0, 170, 80);
+    pop();
+
+    push();
+    translate(125, 125);
+    rotate(QUARTER_PI);
+    noStroke();
+    fill('white');
+    ellipse(0, 0, 80, 170);
+    pop();
+
+    push();
+    translate(275, 275);
+    rotate(QUARTER_PI);
+    noStroke();
+    fill('white');
+    ellipse(0, 0, 80, 170)
+    pop();
+
+    push();
+    translate(125, 275);
+    rotate(QUARTER_PI);
+    noStroke();
+    fill('white');
+    ellipse(0, 0, 170, 80);
+    pop();
+}
+
