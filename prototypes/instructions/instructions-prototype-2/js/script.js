d730e1b812a078621a0e16762878c7f79c537fb0
Sofia-AA34 <s_allash@live.concordia.ca>
2026-09-24 02:40:16 -0400

    added definition to the iris and set up a mouse clicker to the pupil

--- prototypes/instructions/instructions-prototype-2/js/script.js
+++ prototypes/instructions/instructions-prototype-2/js/script.js
@@ -9,6 +9,11 @@
 "use strict";
 
 /**
+ * Value for the mouse clicked function
+ */
+let value = 0
+
+/**
  * Creates the canvas
 */
 function setup() {
@@ -16,7 +21,7 @@ function setup() {
 }
 
 /**
- * Draws the iris
+ * Draws the eye
 */
 function draw() {
     //black background
@@ -31,26 +36,53 @@ function draw() {
 function drawEye() {
     drawIris();
     drawPupil();
-
 }
 
 /**
  * Draw the iris of the eye 
  */
 function drawIris() {
+    //draw the shape of the iris
     push();
     fill(128, 128, 128);
     circle(200, 200, 400);
+    pop();
+
+    //add more definition to the iris by adding more circles around it
+    push();
+    noFill();
+    stroke(24, 24, 24);
+    strokeWeight(10);
+    circle(200, 200, 175);
+    circle(200, 200, 200);
+    circle(200, 200, 225);
+    circle(200, 200, 250);
+    circle(200, 200, 275);
+    circle(200, 200, 300);
+    circle(200, 200, 325);
+    circle(200, 200, 350);
+    circle(200, 200, 375);
     pop();
 }
 
 /**
  * Draw the pupil of the eye
  */
+// draw the shape of the pupil
 function drawPupil() {
+
+    //draw the pupil
     push();
-    fill(0, 0, 0);
+    fill(value);
     circle(200, 200, 150);
     pop();
+}
+// toggle the iris color when the user clicks 
+function mouseClicked() {
+    if (value === 0) {
+        value = 255
+    } else {
+        value = 0;
+    }
 }
 
